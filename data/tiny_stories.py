adf6da9b789059ac791bd5c2ac89be96d9465cc2
Cameron Wheeler <Cam.Wheeler@ed.ac.uk>
2026-07-01 17:19:26 +0100

    dataset download and dotenv addition

--- data/tiny_stories.py
+++ data/tiny_stories.py
@@ -1,4 +1,21 @@
-"""Code for downloading the Tiny Stories dataset."""
+"""
+
+Code for downloading the Tiny Stories dataset.
+
+Run this script on its own to download the dataset.
+
+```bash
+python3 tiny_stories.py
+```
+"""
 
 from datasets import load_dataset
+from dotenv import load_dotenv
 
+DATASET_NAME = "roneneldan/TinyStories"
+
+if __name__ == "__main__":
+    load_dotenv() # Load environment variables from .env file, it will download regardless.
+    print("Loading dataset...")
+    dataset = load_dataset(DATASET_NAME, cache_dir="./cache")
+    print(dataset)
