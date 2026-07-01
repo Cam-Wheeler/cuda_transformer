957e7d1229184bf21b9504528e84dd9d7395e10f
Cameron Wheeler <Cam.Wheeler@ed.ac.uk>
2026-07-01 17:50:00 +0100

    Decided on QWEN style model, added rough structure

---
+++ models/pytorch_transformer.py
@@ -0,0 +1,27 @@
+"""
+Base Pytorch implementation of QWEN-3 Style Transformer model.
+
+Architecture Overview:
+    - QWEN-3 style transformer
+    - 0.6B parameters
+    - Grouped Query Attention (GQA)
+    - Rotary Position Embedding (RoPE)
+    - RMSNorm with pre-normalisation
+    - QK-Norm
+    - Tie embeddings
+    - Casual masking
+    - We will use the QWEN-3 tokeniser to tokenise the text.
+
+Model Architecture:
+    - 28 Layers
+    - Heads (Q / KV): 16/8
+    - Head dim: 128
+    - Hidden activation: SELU
+    - Hidden dim: 1024
+    - Num Q heads: 16
+    - Num KV heads: 8
+    - RMS Norms EPS: 1e-06
+    - ROPE Theta: 1000000
+
+This model is based on the QWEN-3 paper: https://arxiv.org/abs/2505.09388
+"""
