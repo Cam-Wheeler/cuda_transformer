489d02f0a4681c6d617e76b249a3b1d2cbc98e05
Cameron Wheeler <Cam.Wheeler@ed.ac.uk>
2026-07-02 18:52:48 +0100

    Very rough skeleton of QWEN after reading the paper.

--- models/pytorch_transformer.py
+++ models/pytorch_transformer.py
@@ -24,18 +24,67 @@ Model Architecture:
     - ROPE Theta: 1000000
 
 This model is based on the QWEN-3 paper: https://arxiv.org/abs/2505.09388
+
+We make use of many more papers as well:
+
+- GQA: https://arxiv.org/abs/2305.13245
+- RoPE: https://arxiv.org/abs/2104.09864
+- RMSNorm: https://arxiv.org/abs/1910.07467
+- SWIGLU: https://arxiv.org/abs/2002.05202
+
+I also used several other resources to help me understand and build!
+
+- https://cameronrwolfe.substack.com/p/decoder-only-transformers-the-workhorse
 """
 
 import torch
 from torch import nn
+from torch.nn import functional as F
+
 
 # Local imports
 from config import QWEN3Config
 
 
 class QWEN3(nn.Module):
-    
+    """ A QWEN-3 style transformer model."""
+
     def __init__(self, config: QWEN3Config):
         super().__init__()
         self.config = config
         pass
+
+class QWEN3Embedding(nn.Module):
+    """ The embedding layer for the model."""
+    pass
+
+class QWEN3RoPE(nn.Module):
+    """ RoPE embedding for the model."""
+    pass
+
+class QWEN3Block(nn.Module):
+    """ 
+    A single QWEN-3 decoder block.
+    This block consists of:
+    - RMSNorm
+    - GQA self-attention (RoPE included)
+    - RMSNorm
+    - MLP (SWIGLU)
+    """
+    pass
+
+class QWEN3RMSNorm(nn.Module):
+    """ The RMSNorm for the model."""
+    pass
+
+class QWEN3GQAAttention(nn.Module):
+    """ The GQA self-attention for the model."""
+    pass
+
+class QWEN3MLP(nn.Module):
+    """ The MLP layer for the model. This uses SWIGLU."""
+    pass
+
+class QWEN3LMHead(nn.Module):
+    """ The LM head for the model."""
+    pass
