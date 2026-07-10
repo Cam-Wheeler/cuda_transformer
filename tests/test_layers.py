87fea9ff044bdb80896a74f8ead259945ef33e57
Cam-Wheeler <cam010100@hotmail.co.uk>
2026-07-10 19:24:00 +0000

    Addition of the FNN for PyTorch QWEN and simple shape test

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,7 +1,15 @@
 import pytest
+import torch
 
 # Locals
 from models.pytorch_transformer import QWEN3FFN
 
-
-
+class TestQWEN3FFN:
+    def test_forward_shape(self):
+        ffn = QWEN3FFN(
+            embedding_dim=1024,
+            hidden_dim=3072
+        )
+        x = torch.randn(1, 1024)
+        y = ffn(x)
+        assert y.shape == (1, 1024)
