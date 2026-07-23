4268a1b976c7d843ddeb1c01159f966ab721b0f4
Cameron Wheeler <Cam.Wheeler@ed.ac.uk>
2026-07-23 07:24:42 +0100

    Push to change dev machines

---
+++ cudasrc/training/kernels/elementwise.cu
@@ -0,0 +1,41 @@
+/*
+Implementation of element-wise addition and multiplication.
+Main targets are the residual connections and FFN within QWEN.
+*/
+
+
+
+/*
+Forward pass kernel for element-wise addition.
+y[idx] = a[idx] + b[idx]
+
+@param a: Input tensor.
+@param b: Input tensor.
+*/
+
+
+/*
+Backward pass kernel for element-wise addition.
+a[idx] = grad[idx], b[idx] = grad[idx]
+*/
+
+
+/*
+Forward pass kernel for element-wise multiplication.
+y[idx] = a[idx] * b[idx]
+
+@param 
+*/
+
+/*
+Backward pass kernel for element-wise multiplication.
+*/
+
+
+/*
+Lauch code for element-wise addition.
+*/
+
+/*
+Launch code for element-wise multiplication.
+*/
