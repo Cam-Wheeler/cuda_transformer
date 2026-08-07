639ba792864818b55d904896b056a8597bb5e8a8
Cameron Wheeler <Cam.Wheeler@ed.ac.uk>
2026-08-07 06:09:38 +0100

    Wrappers all wired up

--- wrappers/training/batched_matmul.py
+++ wrappers/training/batched_matmul.py
@@ -30,13 +30,51 @@ class BatchedMatMulFunction(Function):
     def forward(ctx, A, B):
         """
         Forward pass.
+
+        Args:
+        - ctx: Context object injected by torch.
+        - A: Matrix A (batch x M x K).
+        - B: Matrix B (batch x K x N)
+
+        Returns:
+        - C: Result of batched matrix multiply (batch x M x N)
         """
-        pass
-    
+        ctx.save_for_backward(A, B)
+
+        # Shapes
+        batch_size, M, _ = A.shape # batch, M, K
+        _, _, N = B.shape # batch, K, N
+
+        C = torch.empty(batch_size, M, N, dtype=A.dtype, device=A.device)
+
+        import custom_training as ct
+        ct.fwd_batched_matmul(A, B, C)
+
+        return C
 
     @staticmethod
     def backward(ctx, grad_out):
         """
         Backward pass.
+
+        Gradient formulas:
+            - grad_A = grad_C @ B^T  (gradient w.r.t. A)
+            - grad_B = A^T @ grad_C  (gradient w.r.t. B)
+
+        Args:
+        - ctx: Context object injected by torch.
+        - grad_out: Gradients with respect to the output of the matmul (batch x M x N).
+
+        Returns:
+        - grad_a: Gradients with respect to A (batch x M x K).
+        - grad_b: Gradients with respect to B (batch x K x N). 
         """
-        pass
+        A, B = ctx.saved_tensors
+
+        grad_a = torch.empty(A.shape, dtype=A.dtype, device=A.device)
+        grad_b = torch.empty(B.shape, dtype=B.dtype, device=B.device)
+
+        import custom_training as ct
+        ct.bwd_batched_matmul(grad_out, A, B, grad_a, grad_b)
+
+        return grad_a, grad_b
--- wrappers/training/matmul.py
+++ wrappers/training/matmul.py
@@ -30,6 +30,14 @@ class MatMulFunction(Function):
     def forward(ctx, A, B):
         """
         Forward pass.
+
+        Args:
+        - ctx: Context object injected by torch.
+        - A: Matrix A (M x K).
+        - B: Matrix B (K x N)
+
+        Returns:
+        - C: Result of matrix multiply (M x N)
         """
         ctx.save_for_backward(A, B)
 
@@ -51,13 +59,22 @@ class MatMulFunction(Function):
         Gradient formulas:
             - grad_A = grad_C @ B^T  (gradient w.r.t. A)
             - grad_B = A^T @ grad_C  (gradient w.r.t. B)
+        
+        Args:
+        - ctx: Context object injected by torch.
+        - grad_out: Gradients with respect to the output of the matmul (M x N).
+
+        Returns:
+        - grad_a: Gradients with respect to A (M x K).
+        - grad_b: Gradients with respect to B (K x N). 
+
         """
         A, B = ctx.saved_tensors
 
-        grad_A = torch.empty(A.shape, dtype=A.dtype, device=A.device)
-        grad_B = torch.empty(B.shape, dtype=B.dtype, device=B.device)
+        grad_a = torch.empty(A.shape, dtype=A.dtype, device=A.device)
+        grad_b = torch.empty(B.shape, dtype=B.dtype, device=B.device)
 
         import custom_training as ct
-        ct.bwd_matmul(grad_out, A, B, grad_A, grad_B)
+        ct.bwd_matmul(grad_out, A, B, grad_a, grad_b)
 
-        return grad_A, grad_B
+        return grad_a, grad_b
